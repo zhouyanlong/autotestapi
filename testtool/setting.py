fd89f3ac67eec35f5ca3a6de434773f5de415830
zhou <zhou4561@qq.com>
2021-04-20 11:00:53 +0800

    修改excel路径

--- testtool/setting.py
+++ testtool/setting.py
@@ -4,8 +4,16 @@ testdir=os.path.dirname(os.path.dirname(__file__))
 sys.path.append(testdir)
 
 configdir=os.path.join(testdir,"config","config.ini")
-testcasedir=os.path.join(testdir,"config","apicase.xlsx")
-testcaseadmindir=os.path.join(testdir,"config","apicaseadmin.xlsx")
+#testcasedir=os.path.join(testdir,"config","apicase.xlsx")
+testcasedir=os.path.join(testdir,"apicase.xlsx")
+#testcaseadmindir=os.path.join(testdir,"config","apicaseadmin.xlsx")
+testcaseadmindir=os.path.join(testdir,"apicaseadmin.xlsx")
 reportdir=os.path.join(testdir,"report")
 logdir=os.path.join(testdir,"log","log.txt")
 testpydir=os.path.join(testdir,"testcase")
+
+
+
+
+if __name__ == '__main__':
+    print(testcasedir,testcaseadmindir)
